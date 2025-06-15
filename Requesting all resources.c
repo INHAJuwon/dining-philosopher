fd60d924bdf63f7bf0987a2b0a1ea33426353192
INHAJuwon <147503909+INHAJuwon@users.noreply.github.com>
2025-06-15 21:41:55 +0900

    Requesting all resources.c

--- Requesting all resources.c
+++ Requesting all resources.c
@@ -1,1 +1,62 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+#define NUM 5
 
+sem_t forks[NUM];
+sem_t once; // 한 번에 좌, 우 fork를 같이 들기 위한 semaphore
+
+void pickup(int philosopher_num){
+	sem_wait(&forks[philosopher_num%NUM]);
+}
+
+void putdown(int philosopher_num){
+	sem_post(&forks[philosopher_num%NUM]);
+}
+
+void thinking(int philosopher_num){
+	printf("philosopher %d is thinking\n", philosopher_num);
+	return;
+}
+void eating(int philosopher_num){
+	printf("philosopher %d is eating\n", philosopher_num);
+	return;
+}
+void* philosopher(void* arg){
+	int philosopher_num;
+	philosopher_num=(unsigned long int)arg;
+	while(1){
+		sem_wait(&once); // deadlock을 피하기 위해 lock 하고 한 번에 양 fork를 동시에 pickup, 점유,대기를 막을 수는 있지만 concurrency를 해친다.
+		pickup(philosopher_num);
+		printf("philosopher %d picks up the fork %d\n",philosopher_num, philosopher_num);
+		pickup(philosopher_num+1);
+		printf("philosopher %d picks up the fork %d\n",philosopher_num, (philosopher_num+1)%NUM);
+		sem_post(&once); // 이미 두 fork를 픽업하여 점유, 대기 상태가 걸릴 리는 없으니 unlock
+		eating(philosopher_num);
+		putdown(philosopher_num+1);
+		printf("philosopher %d puts down the fork %d\n",philosopher_num, (philosopher_num+1)%NUM);
+		putdown(philosopher_num);
+		printf("philosopher %d puts down the fork %d\n",philosopher_num, philosopher_num);
+		thinking(philosopher_num);
+	}
+	return NULL;
+}
+
+int main(){
+	pthread_t threads[NUM];
+
+	for(int i=0;i<NUM;i++){
+		sem_init(&forks[i],0,1);
+	}
+	sem_init(&once,0,1);
+
+	for(unsigned long int i=0;i<NUM;i++){
+		pthread_create(&threads[i],NULL,philosopher,(void*)i);
+	}
+
+	for(int i=0;i<NUM;i++){
+		pthread_join(threads[i],NULL);
+	}
+	printf("NO DEADLOCK\n");
+	return 0;
+}
