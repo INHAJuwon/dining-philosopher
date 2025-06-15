f6d31da879a113d4110bc3fd6e85b50e85289409
INHAJuwon <147503909+INHAJuwon@users.noreply.github.com>
2025-06-15 20:54:38 +0900

    banker.c

--- banker.c
+++ banker.c
@@ -6,10 +6,10 @@
 
 sem_t forks[NUM];
 sem_t m;
-int V[NUM]={1,1,1,1,1};
-int claim[NUM][NUM]={{1,1,0,0,0},{0,1,1,0,0},{0,0,1,1,0},{0,0,0,1,1},{1,0,0,0,1}};
-int allocation[NUM][NUM]={0};
-int need[NUM][NUM]={{1,1,0,0,0},{0,1,1,0,0},{0,0,1,1,0},{0,0,0,1,1},{1,0,0,0,1}};
+int V[NUM]={1,1,1,1,1}; //사용가능한 남은 자원
+int claim[NUM][NUM]={{1,1,0,0,0},{0,1,1,0,0},{0,0,1,1,0},{0,0,0,1,1},{1,0,0,0,1}}; // 각 philosopher마다 요구하는 자원량
+int allocation[NUM][NUM]={0}; //할당된 자원
+int need[NUM][NUM]={{1,1,0,0,0},{0,1,1,0,0},{0,0,1,1,0},{0,0,0,1,1},{1,0,0,0,1}}; // C-A
 
 void pickup(int philosopher_num){
 	sem_wait(&forks[philosopher_num%NUM]);
@@ -20,14 +20,14 @@ void putdown(int philosopher_num){
 }
 
 bool check(int philosopher_num){
-	sem_wait(&m);
+	sem_wait(&m); // 상호 배제
 	for(int i=0;i<NUM;i++){
-		if(need[philosopher_num][i]>V[i]){
+		if(need[philosopher_num][i]>V[i]){ //만약 C-A가 남은 자원보다 크다면 할당할 수 없기에 false
 			sem_post(&m);
 			return false;
 		}
 	}
-	for(int i=0;i<NUM;i++){
+	for(int i=0;i<NUM;i++){ //할당한다면 남은 자원은 C-A만큼 감소, 할당은 C-A만큼 증강, 할당 받았기에 C-A=0
 		V[i]-=need[philosopher_num][i];
 		allocation[philosopher_num][i]+=need[philosopher_num][i];
 		need[philosopher_num][i]=0;
@@ -42,7 +42,7 @@ bool check(int philosopher_num){
 
 void release(int philosopher_num){
 	sem_wait(&m);
-	for(int i=0;i<NUM;i++){
+	for(int i=0;i<NUM;i++){ //할당했던 자원들을 다시 원상복구
 		V[i]+=need[philosopher_num][i];
 		need[philosopher_num][i]-=need[philosopher_num][i];
 		allocation[philosopher_num][i]=0;
@@ -66,11 +66,11 @@ void* philosopher(void* arg){
 	int philosopher_num;
 	philosopher_num=(unsigned long int)arg;
 	while(true){
-		if(check(philosopher_num)){
+		if(check(philosopher_num)){ // 할당 가능한 자원이라면 eating 가능
 			eating(philosopher_num);
 			release(philosopher_num);
 		}
-		thinking(philosopher_num);
+		thinking(philosopher_num); 
 	}
 	return NULL;
 }
